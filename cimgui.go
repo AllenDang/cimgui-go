f71b71445b4fd7f8bd2ef8989fab1be3d9da543b
Allen <allengnr@gmail.com>
2022-08-09 14:55:19 +0800

    Change LDFLAGS for macos

--- cimgui.go
+++ cimgui.go
@@ -3,8 +3,8 @@ package cimgui
 // #cgo CPPFLAGS: -DCIMGUI_DEFINE_ENUMS_AND_STRUCTS
 // #cgo amd64,linux LDFLAGS: ${SRCDIR}/lib/linux/x64/cimgui.a
 // #cgo amd64,windows LDFLAGS: -L${SRCDIR}/lib/windows/x64 -lcimgui
-// #cgo amd64,darwin LDFLAGS: ${SRCDIR}/lib/macos/x64/cimgui.a -framework Cocoa -framework OpenGL -framework ApplicationServices -framework Carbon
-// #cgo arm64,darwin LDFLAGS: ${SRCDIR}/lib/macos/arm64/cimgui.a -framework Cocoa -framework OpenGL -framework ApplicationServices -framework Carbon
+// #cgo amd64,darwin LDFLAGS: ${SRCDIR}/lib/macos/x64/cimgui.a
+// #cgo arm64,darwin LDFLAGS: ${SRCDIR}/lib/macos/arm64/cimgui.a
 // #include "cimgui_wrapper.h"
 import "C"
 
