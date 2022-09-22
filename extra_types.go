4c34a12eeff4a07bdd279bed17a5a1ffa7f18f06
neclepsio <neclepsio@gmail.com>
2022-09-22 11:09:50 +0200

    ImVec2 and ImVec4 arithmetic, ImColor conversions

--- extra_types.go
+++ extra_types.go
@@ -4,6 +4,8 @@ package cimgui
 // #include "cimplot_wrapper.h"
 import "C"
 
+import "image/color"
+
 type ImWchar C.uint
 type ImGuiID C.ImGuiID
 type ImTextureID uintptr
@@ -42,6 +44,18 @@ func (vec *ImVec2) wrap() (out *C.ImVec2, finisher func()) {
 		finisher = func() {}
 	}
 	return
+}
+
+func (vec ImVec2) Plus(other ImVec2) ImVec2 {
+	return ImVec2{X: vec.X + other.X, Y: vec.Y + other.Y}
+}
+
+func (vec ImVec2) Minus(other ImVec2) ImVec2 {
+	return ImVec2{X: vec.X - other.X, Y: vec.Y - other.Y}
+}
+
+func (vec ImVec2) Times(t float32) ImVec2 {
+	return ImVec2{X: vec.X * t, Y: vec.Y * t}
 }
 
 type ImVec4 struct {
@@ -92,6 +106,33 @@ func (vec *ImVec4) wrap() (out *C.ImVec4, finisher func()) {
 	return
 }
 
+func (vec ImVec4) Plus(other ImVec4) ImVec4 {
+	return ImVec4{
+		X: vec.X + other.X,
+		Y: vec.Y + other.Y,
+		Z: vec.Z + other.Z,
+		W: vec.W + other.W,
+	}
+}
+
+func (vec ImVec4) Minus(other ImVec4) ImVec4 {
+	return ImVec4{
+		X: vec.X - other.X,
+		Y: vec.Y - other.Y,
+		Z: vec.Z - other.Z,
+		W: vec.W - other.W,
+	}
+}
+
+func (vec ImVec4) Times(t float32) ImVec4 {
+	return ImVec4{
+		X: vec.X * t,
+		Y: vec.Y * t,
+		Z: vec.Z * t,
+		W: vec.W * t,
+	}
+}
+
 type ImColor struct {
 	Value ImVec4
 }
@@ -105,6 +146,25 @@ func NewImColor(x, y, z, w float32) ImColor {
 			W: w,
 		},
 	}
+}
+
+func NewImColorFromPacked(v uint32) ImColor {
+	return NewImColor(
+		float32(0xff&(v>>0))/255,
+		float32(0xff&(v>>8))/255,
+		float32(0xff&(v>>16))/255,
+		float32(0xff&(v>>24))/255,
+	)
+}
+
+func NewImColorFromColor(c color.Color) ImColor {
+	nc := color.NRGBAModel.Convert(c).(color.NRGBA)
+	return NewImColor(
+		float32(nc.R)/0xffff,
+		float32(nc.G)/0xffff,
+		float32(nc.B)/0xffff,
+		float32(nc.A)/0xffff,
+	)
 }
 
 func (i ImColor) toC() C.ImColor {
@@ -126,6 +186,33 @@ func (i *ImColor) wrap() (out *C.ImColor, finisher func()) {
 		finisher = func() {}
 	}
 	return
+}
+
+func colorComponent(v float32) uint8 {
+	r := int(v*255 + 0.5)
+	if r < 0 {
+		return 0
+	}
+	if r > 255 {
+		return 255
+	}
+	return uint8(r)
+}
+
+func (i ImColor) Pack() uint32 {
+	return uint32(colorComponent(i.Value.X))<<0 |
+		uint32(colorComponent(i.Value.Y))<<8 |
+		uint32(colorComponent(i.Value.Z))<<16 |
+		uint32(colorComponent(i.Value.W))<<24
+}
+
+func (i ImColor) Color() color.Color {
+	return color.NRGBA{
+		R: colorComponent(i.Value.X),
+		G: colorComponent(i.Value.Y),
+		B: colorComponent(i.Value.Z),
+		A: colorComponent(i.Value.W),
+	}
 }
 
 type ImRect struct {
