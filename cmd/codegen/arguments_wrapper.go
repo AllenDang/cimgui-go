f6dc0060dc7ee3258374dcc2f42a457962155719
gucio321 <73652197+gucio321@users.noreply.github.com>
2023-01-17 14:15:43 +0100

    codegen: replace []*type with *[]type in argument wrappers

--- cmd/codegen/arguments_wrapper.go
+++ cmd/codegen/arguments_wrapper.go
@@ -228,19 +228,19 @@ func() {
 	}
 }
 
-// C.int*, C.int[] -> []*int32
+// C.int*, C.int[] -> *[]int32
 func simplePtrSliceW(cArrayType, goArrayType string) argumentWrapper {
 	return func(arg ArgDef) ArgumentWrapperData {
 		return ArgumentWrapperData{
-			ArgType: fmt.Sprintf("[]*%s", goArrayType),
-			ArgDef: fmt.Sprintf(`%[1]sArg := make([]%[2]s, len(%[1]s))
-for i, %[1]sV := range %[1]s {
-  %[1]sArg[i] = %[2]s(*%[1]sV)
+			ArgType: fmt.Sprintf("*[]%s", goArrayType),
+			ArgDef: fmt.Sprintf(`%[1]sArg := make([]%[2]s, len(*%[1]s))
+for i, %[1]sV := range *%[1]s {
+  %[1]sArg[i] = %[2]s(%[1]sV)
 }
 `, arg.Name, cArrayType, goArrayType),
 			Finalizer: fmt.Sprintf(`func() {
   for i, %[1]sV := range %[1]sArg {
-    *%[1]s[i] = %[3]s(%[1]sV)
+    (*%[1]s)[i] = %[3]s(%[1]sV)
   }
 }()
 `, arg.Name, cArrayType, goArrayType),
