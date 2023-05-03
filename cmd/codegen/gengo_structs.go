fa6e5fcc707af885b404f1c9ce748aeba8c9de51
gucio321 <73652197+gucio321@users.noreply.github.com>
2023-05-03 13:46:27 +0200

    generator: generate struct fields

--- cmd/codegen/gengo_structs.go
+++ cmd/codegen/gengo_structs.go
@@ -1,5 +1,6 @@
 package main
 
+import "C"
 import (
 	"fmt"
 	"os"
@@ -26,21 +27,7 @@ import "unsafe"
 		}
 
 		sb.WriteString(fmt.Sprintf("%s\n", s.CommentAbove))
-		sb.WriteString(fmt.Sprintf(`type %[1]s uintptr
-
-func (data %[1]s) handle() *C.%[2]s {
-  return (*C.%[2]s)(unsafe.Pointer(data))
-}
-
-func (data %[1]s) c() C.%[2]s {
-  return *(data.handle())
-}
-
-func new%[1]sFromC(cvalue C.%[2]s) %[1]s {
-  return %[1]s(unsafe.Pointer(&cvalue))
-}
-
-`, renameGoIdentifier(s.Name), s.Name))
+		generateStruct(s, &sb)
 
 		structNames = append(structNames, s.Name)
 	}
@@ -55,4 +42,103 @@ func new%[1]sFromC(cvalue C.%[2]s) %[1]s {
 	_, _ = structFile.WriteString(sb.String())
 
 	return structNames
+}
+
+func generateStruct(s StructDef, sb *strings.Builder) {
+	type wrapper struct {
+		fromC returnWrapper
+		toC   argumentWrapper
+	}
+
+	var wrappers = make(map[int]wrapper)
+	var isTODO bool // this will become true if some field is not implemented (e.g. union {...})
+	var structBody *strings.Builder = &strings.Builder{}
+
+	fmt.Fprintf(sb, "type %s struct {\n", renameGoIdentifier(s.Name))
+	// Generate struct fields:
+	for i, field := range s.Members {
+		if field.Name == "" { // <- this means that type is union or something like that
+			isTODO = true
+			// reset struct body and fill it with temporary data
+			structBody = &strings.Builder{}
+			fmt.Fprint(structBody, `
+// TODO: contains unsupported fields
+data uintptr
+`)
+			continue
+		}
+
+		typeName := ""
+		switch field.Type {
+		default:
+			wrappers[i] = wrapper{
+				fromC: simpleR("uintptr"),
+				toC:   simpleW("uintptr", field.Name),
+			}
+
+			typeName = "uintptr"
+		}
+
+		fmt.Fprintf(structBody, "%s %s\n", field.Name, typeName)
+	}
+
+	fmt.Fprintln(sb, structBody)
+
+	fmt.Fprint(sb, "}\n")
+
+	// handlers:
+	fmt.Fprintf(sb, `
+func (data %[1]s) handle() *C.%[2]s {
+`, renameGoIdentifier(s.Name), s.Name)
+
+	if isTODO {
+		fmt.Fprintf(sb, "return (*C.%s)(unsafe.Pointer(data))", s.Name)
+	} else {
+		fmt.Fprintf(sb, "result := new(C.%s)\n", s.Name)
+		for i, m := range s.Members {
+			wr := wrappers[i].toC(ArgDef{
+				Name: fmt.Sprintf("data.%s", m.Name),
+				Type: "uintptr", // TODO
+			})
+
+			fmt.Fprintf(sb, "%s\nresult.%s = %s\n", wr.ArgDef, s.Members[i].Name, wr.VarName)
+		}
+
+		fmt.Fprintf(sb, "return result\n")
+	}
+
+	fmt.Fprintf(sb, "}\n")
+
+	fmt.Fprintf(sb, `
+	func (data %[1]s) c() C.%[2]s {
+		return *(data.handle())
+	}
+`, renameGoIdentifier(s.Name), s.Name)
+
+	// release func:
+	fmt.Fprintf(sb, `
+func (data %[1]s) release(result *C.%[2]s) {
+`, renameGoIdentifier(s.Name), s.Name)
+
+	if isTODO {
+		fmt.Fprintf(sb, "C.free(unsafe.Pointer(result))")
+	} else {
+		for i, m := range s.Members {
+			wr := wrappers[i].toC(ArgDef{
+				Name: fmt.Sprintf("data.%s", m.Name),
+				Type: "uintptr", // TODO
+			})
+
+			fmt.Fprintf(sb, "%s\n", wr.Finalizer)
+		}
+	}
+
+	fmt.Fprintf(sb, "}\n")
+
+	sb.WriteString(fmt.Sprintf(`
+func new%[1]sFromC(cvalue C.%[2]s) %[1]s {
+  return %[1]s(unsafe.Pointer(&cvalue))
+}
+
+`, renameGoIdentifier(s.Name), s.Name))
 }
