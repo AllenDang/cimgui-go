b843d787851c28a8f2cb27420631a2d48c6c66d3
gucio321 <73652197+gucio321@users.noreply.github.com>
2023-08-31 22:19:03 +0200

    attempt to fix workflow

--- cimgui.go
+++ cimgui.go
@@ -6,4 +6,5 @@ package imgui
 // #cgo amd64,windows LDFLAGS: -L${SRCDIR}/lib/windows/x64 -l:cimgui.a
 // #cgo amd64,darwin LDFLAGS: ${SRCDIR}/lib/macos/x64/cimgui.a
 // #cgo arm64,darwin LDFLAGS: ${SRCDIR}/lib/macos/arm64/cimgui.a
+// #pragma GCC diagnostic ignored "-fpermissive"
 import "C"
