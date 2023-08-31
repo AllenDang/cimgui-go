b9887b4f417719b7a42712db5e05419eac03b140
gucio321 <73652197+gucio321@users.noreply.github.com>
2023-08-31 22:46:13 +0200

    revert last commits

--- cimgui.go
+++ cimgui.go
@@ -6,5 +6,4 @@ package imgui
 // #cgo amd64,windows LDFLAGS: -L${SRCDIR}/lib/windows/x64 -l:cimgui.a
 // #cgo amd64,darwin LDFLAGS: ${SRCDIR}/lib/macos/x64/cimgui.a
 // #cgo arm64,darwin LDFLAGS: ${SRCDIR}/lib/macos/arm64/cimgui.a
-// #pragma GCC diagnostic ignored "-pedantic"
 import "C"
