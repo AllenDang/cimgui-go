3424aa63f013996164f1500765ea806c7a83e67f
gucio321 <gucio321@protonmail.com>
2024-02-25 20:42:47 +0100

    thirdparty: add dummy files

---
+++ thirdparty.go
@@ -0,0 +1,9 @@
+//go:build required
+// +build required
+
+package imgui
+
+import (
+	"github.com/AllenDang/cimgui-go/thirdparty/SDL/include"
+	"github.com/AllenDang/cimgui-go/thirdparty/glfw/include/GLFW"
+)
---
+++ thirdparty/glfw/include/GLFW/dummy.go
@@ -0,0 +1,3 @@
+// +build required
+
+package dummy
