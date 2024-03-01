8f1c5d79834f300df3d594349ea509cf51ea8978
gucio321 <gucio321@protonmail.com>
2024-03-01 17:20:41 +0100

    backend: make backendCExposer optional part of Backend interface

--- backend.go
+++ backend.go
@@ -11,6 +11,8 @@ package imgui
 import "C"
 
 import (
+	"errors"
+	"fmt"
 	"image"
 	"unsafe"
 )
@@ -85,9 +87,11 @@ func sizeCallback(_ unsafe.Pointer, w, h C.int) {
 	}
 }
 
-type DropCallback func([]string)
-type KeyCallback func(key, scanCode, action, mods int)
-type SizeChangeCallback func(w, h int)
+type (
+	DropCallback       func([]string)
+	KeyCallback        func(key, scanCode, action, mods int)
+	SizeChangeCallback func(w, h int)
+)
 
 type WindowCloseCallback[BackendFlagsT ~int] func(b Backend[BackendFlagsT])
 
@@ -142,7 +146,6 @@ type Backend[BackendFlagsT ~int] interface {
 
 	CreateWindow(title string, width, height int)
 
-	backendCExpose
 	TextureManager
 }
 
@@ -157,7 +160,6 @@ type TextureManager interface {
 }
 
 type backendCExpose interface {
-
 	// for C callbacks
 	// What happens here is a bit tricky:
 	// - user sets these callbacks via Set* methods of the backend
@@ -179,8 +181,24 @@ type backendCExpose interface {
 	sizeCallback() SizeChangeCallback
 }
 
-func CreateBackend[BackendFlagsT ~int](backend Backend[BackendFlagsT]) Backend[BackendFlagsT] {
-	currentBackend = backend
+var CExposerError error = errors.New("CreateBackend was unable to extract C API Exposer from your backend. This is an error if you want to use C-related backend.")
+
+// CreateBackend should to be called before using any cimgui methods.
+// This enables the following features:
+//   - C-related callbacks: if your backend implements C functions, this will export some methods
+//     from your backend to C.
+//   - Advanced Texture Loading stuff. GL texture loading is a little complex. texture.go has some high-level logic.
+//
+// Error:
+//   - Error could be CExposerError when your backend does not support C. THIS MAY STILL WORK CORRECTLY.
+//     Specificly, this may happen if you use some backend that is not part of cimgui-go. In such a case
+//     just ignore the error.
+func CreateBackend[BackendFlagsT ~int](backend Backend[BackendFlagsT]) (sameBackend Backend[BackendFlagsT], err error) {
+	var ok bool
+	if currentBackend, ok = backend.(backendCExpose); !ok {
+		err = fmt.Errorf("%T does not implement %T: %w", backend, currentBackend, CExposerError)
+	}
+
 	textureManager = backend
-	return backend
+	return backend, err
 }
