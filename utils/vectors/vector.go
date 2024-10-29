7d6477607be2d5ac780c8cd33fbf938e5c48d423
gucio321 <gucio321@protonmail.com>
2024-10-29 10:07:32 +0100

    refactor(vectors): now takes plain value not a pointer as type parameter
    This will allow to add some more useful API.
    progress on #269

--- utils/vectors/vector.go
+++ utils/vectors/vector.go
@@ -6,11 +6,11 @@ import "runtime"
 type Vector[T any] struct {
 	Size     int
 	Capacity int
-	Data     T
+	Data     *T
 	pinner   *runtime.Pinner
 }
 
-func NewVectorFromC[T any, CINT ~int32](size, capacity CINT, data T) Vector[T] {
+func NewVectorFromC[T any, CINT ~int32](size, capacity CINT, data *T) Vector[T] {
 	return Vector[T]{Size: int(size), Capacity: int(capacity), Data: data, pinner: &runtime.Pinner{}}
 }
 
