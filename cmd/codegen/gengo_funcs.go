530ce5ea54e66c6f264cec9bd712ef4a65a6ced9
gucio321 <gucio321@protonmail.com>
2025-06-03 14:45:41 +0200

    codegen(logger): Use Warn instead of Error

--- cmd/codegen/gengo_funcs.go
+++ cmd/codegen/gengo_funcs.go
@@ -87,7 +87,7 @@ func GenerateGoFuncs(
 		// stop, when the function should not be generated
 		if !generator.shouldGenerate {
 			if context.flags.ShowNotGenerated {
-				glg.Errorf("not generated: %s%s", f.FuncName, f.Args)
+				glg.Warnf("not generated: %s%s", f.FuncName, f.Args)
 			}
 
 			continue
