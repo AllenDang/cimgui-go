45dbe0266f11f2cd1d4c522cb549db31efd8c906
gucio321 <gucio321@protonmail.com>
2025-06-27 17:02:17 +0200

    ebitenbackend: remove font atlas
    this concept was entirely removed in imgui. This will be now handled through Texture Requests

--- backend/ebiten-backend/txcache.go
+++ backend/ebiten-backend/txcache.go
@@ -6,25 +6,20 @@ import (
 )
 
 type TextureCache interface {
-	FontAtlasTextureID() imgui.TextureID
-	SetFontAtlasTextureID(id imgui.TextureID)
 	GetTexture(id imgui.TextureID) *ebiten.Image
 	GetGameTexture(id imgui.TextureID) (ebiten.Game, bool)
 	ForEachGame(f func(id imgui.TextureID, game ebiten.Game, target *ebiten.Image))
 	SetTexture(id imgui.TextureID, img *ebiten.Image)
 	SetGameTexture(id imgui.TextureID, img ebiten.Game)
 	RemoveTexture(id imgui.TextureID)
-	ResetFontAtlasCache(filter ebiten.Filter)
 	NextId() int
 }
 
 type textureCache struct {
-	startIndex     int
-	fontAtlasID    imgui.TextureID
-	fontAtlasImage *ebiten.Image
-	cache          map[imgui.TextureID]*ebiten.Image
-	cacheGame      map[imgui.TextureID]ebiten.Game
-	dfilter        ebiten.Filter
+	startIndex int
+	cache      map[imgui.TextureID]*ebiten.Image
+	cacheGame  map[imgui.TextureID]ebiten.Game
+	dfilter    ebiten.Filter
 }
 
 var _ TextureCache = (*textureCache)(nil)
@@ -33,30 +28,7 @@ func (c *textureCache) NextId() int {
 	return len(c.cache) + c.startIndex
 }
 
-func (c *textureCache) getFontAtlas() *ebiten.Image {
-	if c.fontAtlasImage == nil {
-		pixels, width, height, _ := imgui.CurrentIO().Fonts().GetTextureDataAsRGBA32()
-		c.fontAtlasImage = getTexture(pixels, width, height)
-		c.SetTexture(c.fontAtlasID, c.fontAtlasImage)
-		imgui.CurrentIO().Fonts().SetTexID(c.fontAtlasID)
-	}
-
-	return c.fontAtlasImage
-}
-
-func (c *textureCache) FontAtlasTextureID() imgui.TextureID {
-	return c.fontAtlasID
-}
-
-func (c *textureCache) SetFontAtlasTextureID(id imgui.TextureID) {
-	c.fontAtlasID = id
-}
-
 func (c *textureCache) GetTexture(id imgui.TextureID) *ebiten.Image {
-	if id == c.fontAtlasID {
-		return c.getFontAtlas()
-	}
-
 	if im, ok := c.cache[id]; ok {
 		return im
 	}
@@ -91,17 +63,10 @@ func (c *textureCache) RemoveTexture(id imgui.TextureID) {
 	delete(c.cache, id)
 }
 
-func (c *textureCache) ResetFontAtlasCache(filter ebiten.Filter) {
-	c.fontAtlasImage = nil
-	c.dfilter = filter
-}
-
 func NewCache() TextureCache {
 	return &textureCache{
-		startIndex:     2,
-		fontAtlasID:    imgui.TextureID(1),
-		cache:          make(map[imgui.TextureID]*ebiten.Image),
-		cacheGame:      make(map[imgui.TextureID]ebiten.Game),
-		fontAtlasImage: nil,
+		startIndex: 2,
+		cache:      make(map[imgui.TextureID]*ebiten.Image),
+		cacheGame:  make(map[imgui.TextureID]ebiten.Game),
 	}
 }
